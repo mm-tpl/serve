5e3c0b32df07a1566783a07a097403af01dfc723
taoqf <tao_qiufeng@126.com>
2020-11-02 14:49:51 +0800

    template

--- src/atoms/anp001/index.ts
+++ src/atoms/anp001/index.ts
@@ -6,7 +6,7 @@ export default async function anp1<T>(sql: string, ...args: unknown[]) {
 	* 数据库返回值
 	*/
 	const [r1603335417] = await (() => {
-		const p1 = 'db001';
+		const p1 = 'prj001';
 		return an14<T>(p1, [sql, args]);
 	})();
 	return r1603335417;
